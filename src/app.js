981b99ad4aa53ae35d9b7ab6280614a1c57435c5
devourer77 <devourer77@gmail.com>
2019-09-05 20:03:50 +0300

    gg

--- src/app.js
+++ src/app.js
@@ -4,3 +4,19 @@ import 'bootstrap';
 import 'lightslider';
 import 'hamburgers';
 
+$(document).ready(function(){
+
+    $('#lightSlider').lightSlider({
+        item:1,
+        adaptiveHeight:false,
+        slideMove:1,
+        speed:750,
+        slideMargin:0,
+        loop:false,
+        rtl:false,
+        vertical: true,
+        verticalHeight: 1082,
+// auto: true,
+    });
+
+});
